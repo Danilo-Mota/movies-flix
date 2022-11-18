b614dcf450a630692e59d23a3cf46259205d2eaf
Danilo Mota Filho <danilo@jera.com.br>
2022-11-18 12:10:29 -0300

    NEW creating project

---
+++ jeraflix_danilo/.dart_tool/flutter_gen/gen_l10n/localization_pt.dart
@@ -0,0 +1,9 @@
+import 'localization.dart';
+
+/// The translations for Portuguese (`pt`).
+class LocalizationPt extends Localization {
+  LocalizationPt([String locale = 'pt']) : super(locale);
+
+  @override
+  String get appTitle => 'Jeraflix - Danilo';
+}
