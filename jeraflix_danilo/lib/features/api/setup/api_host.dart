f15da9845644a20757fa2549de5a4b70c71facd6
Danilo Mota Filho <danilo@jera.com.br>
2022-11-22 15:17:01 -0300

    NEW api setup

---
+++ jeraflix_danilo/lib/features/api/setup/api_host.dart
@@ -0,0 +1,4 @@
+class ApiHost {
+  static const String apiKey = '3b9fba8b00c40dcb824c888f7e7a7405';
+  static String get baseURL => 'https://api.themoviedb.org/3/';
+}
---
+++ jeraflix_danilo/lib/features/api/setup/api_provider.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+import 'api_host.dart';
+import 'endpoint.dart';
+
+typedef Success = void Function(dynamic result);
+typedef Failure = void Function(DioError error);
+
+class ApiProvider {
+  final Dio _dio = Dio()..interceptors.add(PrettyDioLogger());
+
+  Future<void> request({required Endpoint endpoint, Success? success, Failure? failure}) async {
+    _dio.options.method = endpoint.method;
+    _dio.options.baseUrl = ApiHost.baseURL;
+    _dio.options.contentType = endpoint.contentType ?? ContentType.json.value;
+
+    try {
+      final response = await _dio.request(
+        endpoint.path,
+        data: endpoint.data,
+        queryParameters: endpoint.queryParameters,
+      );
+      success?.call(response.data);
+    } on DioError catch (e) {
+      failure?.call(e);
+    }
+  }
+}
