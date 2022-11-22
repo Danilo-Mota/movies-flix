542515573e2c524bddb472d7a3975b0a7cbdc1ab
Danilo Mota Filho <danilo@jera.com.br>
2022-11-22 17:23:44 -0300

    NEW usecase setup

--- jeraflix_danilo/lib/api/routes/movies.dart
+++ jeraflix_danilo/lib/api/routes/movies.dart
@@ -10,7 +10,7 @@ class MoviesRoutes extends MoviesRoutesProtocol {
 
   @override
   void getPopularMovies({Success? success, Failure? failure}) {
-    final queryParameters = {'api_key': ''};
+    final queryParameters = {'api_key': '3b9fba8b00c40dcb824c888f7e7a7405'};
 
     final endpoint = Endpoint(path: '/movie/popular', method: 'GET', queryParameters: queryParameters);
     _provider.request(endpoint: endpoint, success: success, failure: failure);
---
+++ jeraflix_danilo/lib/features/home/use_cases/get_popular_movies_use_case.dart
@@ -0,0 +1,23 @@
+import '../../../api/routes/movies.dart';
+import '../../../api/setup/api_provider.dart';
+
+typedef Error = void Function(ServerError error);
+typedef Success = void Function(List<PopularMovies> popularMovies);
+
+abstract class GetPopularMoviesUseCaseProtocol {
+  void invoke({required Success? success, required Failure? failure});
+}
+
+class GetPopularMoviesUseCase extends GetPopularMoviesUseCaseProtocol {
+  final MoviesRoutes routes;
+
+  GetPopularMoviesUseCase({required this.routes});
+
+  @override
+  void invoke({required Success? success, required Failure? failure}) {
+    routes.getPopularMovies(
+      success: (response) {},
+      failure: (error) {},
+    );
+  }
+}
