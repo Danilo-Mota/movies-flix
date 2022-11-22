3e8682c57a3597f3f455b9df6573ef97c7b879ba
Danilo Mota Filho <danilo@jera.com.br>
2022-11-22 10:14:55 -0300

    UPDATE code review

--- jeraflix_danilo/lib/main.dart
+++ jeraflix_danilo/lib/main.dart
@@ -15,7 +15,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: AppTheme.theme,
-        home: const Scaffold(),
         initialRoute: MobileRouter.initialRoute,
         routes: MobileRouter.routes,
         supportedLocales: Localization.supportedLocales,
