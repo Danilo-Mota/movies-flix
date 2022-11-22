e438c1cf6e8051e4f6d3e7706e385c657c0fea6f
Danilo Mota Filho <danilo@jera.com.br>
2022-11-22 19:05:28 -0300

    UPDATE movies model

--- lib/features/home/use_cases/get_popular_movies_use_case.dart
+++ lib/features/home/use_cases/get_popular_movies_use_case.dart
@@ -1,8 +1,9 @@
+import '../../../model/movies.dart';
 import '../../../api/routes/movies.dart';
 import '../../../api/setup/api_provider.dart';
 
 typedef Error = void Function(ServerError error);
-typedef Success = void Function(List<PopularMovies> popularMovies);
+typedef Success = void Function(List<Movies> popularMovies);
 
 abstract class GetPopularMoviesUseCaseProtocol {
   void invoke({required Success? success, required Failure? failure});
---
+++ lib/model/movies.dart
@@ -0,0 +1,12 @@
+class Movies {
+  final int id;
+  final String title;
+  final double rating;
+  final String posterPath;
+
+  Movies.fromMap(Map<String, dynamic> map)
+      : id = map['id'],
+        title = map['title'],
+        rating = map['rating'],
+        posterPath = map['posterPath'];
+}
