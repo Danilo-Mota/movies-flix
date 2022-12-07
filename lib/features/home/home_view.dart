5bdb03d3ae3cd5b02f98a543d22c907e3fe31a49
Danilo Mota Filho <danilo@jera.com.br>
2022-12-07 17:09:29 -0300

    UPDATE mr changes

--- lib/features/home/home_view.dart
+++ lib/features/home/home_view.dart
@@ -9,19 +9,11 @@ import '../../support/components/section_title_view.dart';
 import '../../support/utils/localize.dart';
 
 abstract class HomeViewModelProtocol extends ChangeNotifier {
-  bool get popularMoviesHasError;
-  bool get popularMoviesIsLoading;
-  String get popularMoviesErrorMessage;
+  bool get moviesHasError;
+  bool get moviesIsLoading;
+  String get moviesErrorMessage;
   List<MovieItemViewModelProtocol> get popularMovies;
-
-  bool get topRatedMoviesHasError;
-  bool get topRatedMoviesIsLoading;
-  String get topRatedMoviesErrorMessage;
   List<MovieItemViewModelProtocol> get topRatedMovies;
-
-  bool get upcomingMoviesHasError;
-  bool get upcomingMoviesIsLoading;
-  String get upcomingMoviesErrorMessage;
   List<MovieItemViewModelProtocol> get upcomingMovies;
 }
 
@@ -32,49 +24,50 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final l10n = Localize.instance.l10n;
-
     return Scaffold(
       extendBodyBehindAppBar: true,
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.only(bottom: 8.0),
-        child: SafeArea(
-          child: AnimatedBuilder(
-            animation: viewModel,
-            builder: (_, __) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  _carouselMoviesWidget(context),
-                  const SizedBox(height: 16),
-                  SectionTitleView(title: l10n.topRatedMoviesLabel),
-                  _topRatedMoviesWidget(),
-                   const SizedBox(height: 16),
-                  SectionTitleView(title: l10n.popularMoviesLabel),
-                   _popularMoviesWidget(),
-                  const SizedBox(height: 16),
-                   SectionTitleView(title: l10n.upcomingMoviesLabel),
-                   _upcomingMoviesWidget(),
-                ],
-              );
-            },
-          ),
+      body: AnimatedBuilder(
+        animation: viewModel,
+        builder: (_, __) {
+          return _handleMoviesStateWidget(context);
+        },
+      ),
+    );
+  }
+
+  Widget _handleMoviesStateWidget(BuildContext context) {
+    final l10n = Localize.instance.l10n;
+
+    if (viewModel.moviesIsLoading) return const LoadingPlaceholderView();
+    if (viewModel.moviesHasError) {
+      return ErrorPlaceholderView(errorMessage: viewModel.moviesErrorMessage);
+    }
+
+    return SingleChildScrollView(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _carouselMoviesWidget(context),
+            const SizedBox(height: 16),
+            SectionTitleView(title: l10n.topRatedMoviesLabel),
+            MovieHorizontalList(movies: viewModel.topRatedMovies),
+            const SizedBox(height: 16),
+            SectionTitleView(title: l10n.popularMoviesLabel),
+            MovieHorizontalList(movies: viewModel.popularMovies),
+            const SizedBox(height: 16),
+            SectionTitleView(title: l10n.upcomingMoviesLabel),
+            MovieHorizontalList(movies: viewModel.upcomingMovies),
+          ],
         ),
       ),
     );
   }
 
   Widget _carouselMoviesWidget(BuildContext context) {
-    final height = MediaQuery.of(context).size.height;
-
-    if (viewModel.popularMoviesIsLoading) return const LoadingPlaceholderView();
-    if (viewModel.popularMoviesHasError) {
-      return ErrorPlaceholderView(errorMessage: viewModel.popularMoviesErrorMessage);
-    }
-
     return CarouselSlider(
       options: CarouselOptions(
-        //height: height * 0.55,
         aspectRatio: 0.68,
         viewportFraction: 1.0,
         autoPlay: true,
@@ -88,32 +81,5 @@ class HomeView extends StatelessWidget {
         },
       ).toList(),
     );
-  }
-
-  Widget _popularMoviesWidget() {
-    if (viewModel.popularMoviesIsLoading) return const LoadingPlaceholderView();
-    if (viewModel.popularMoviesHasError) {
-      return ErrorPlaceholderView(errorMessage: viewModel.popularMoviesErrorMessage);
-    }
-
-    return MovieHorizontalList(movies: viewModel.popularMovies);
-  }
-
-  Widget _topRatedMoviesWidget() {
-    if (viewModel.topRatedMoviesIsLoading) return const LoadingPlaceholderView();
-    if (viewModel.topRatedMoviesHasError) {
-      return ErrorPlaceholderView(errorMessage: viewModel.topRatedMoviesErrorMessage);
-    }
-
-    return MovieHorizontalList(movies: viewModel.topRatedMovies);
-  }
-
-  Widget _upcomingMoviesWidget() {
-    if (viewModel.upcomingMoviesIsLoading) return const LoadingPlaceholderView();
-    if (viewModel.upcomingMoviesHasError) {
-      return ErrorPlaceholderView(errorMessage: viewModel.upcomingMoviesErrorMessage);
-    }
-
-    return MovieHorizontalList(movies: viewModel.upcomingMovies);
   }
 }
