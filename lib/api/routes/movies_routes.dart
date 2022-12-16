c841aef70c5e9736939a2d9820cb5c6502e16a37
Danilo Mota Filho <danilo@jera.com.br>
2022-12-16 11:38:49 -0300

    UPDATE search setup

--- lib/api/routes/movies_routes.dart
+++ lib/api/routes/movies_routes.dart
@@ -5,6 +5,7 @@ abstract class MoviesRoutesProtocol {
   void getPopularMovies({Success? success, Failure? failure});
   void getTopRatedMovies({Success? success, Failure? failure});
   void getUpcomingMovies({Success? success, Failure? failure});
+  void getSearchMovies({required String searchText, Success? success, Failure? failure});
 }
 
 class MoviesRoutes extends MoviesRoutesProtocol {
@@ -25,6 +26,12 @@ class MoviesRoutes extends MoviesRoutesProtocol {
   @override
   void getUpcomingMovies({Success? success, Failure? failure}) {
     final endpoint = Endpoint(path: '/movie/upcoming', method: 'GET');
+    _provider.request(endpoint: endpoint, success: success, failure: failure);
+  }
+
+  @override
+  void getSearchMovies({required String searchText, Success? success, Failure? failure}) {
+    final endpoint = Endpoint(path: '/search/movie', method: 'GET', queryParameters: {'query' : searchText});
     _provider.request(endpoint: endpoint, success: success, failure: failure);
   }
 }
---
+++ lib/features/search/search_view_controller.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SearchViewController extends StatefulWidget {
+  const SearchViewController({super.key});
+
+  @override
+  State<SearchViewController> createState() => _SearchViewControllerState();
+}
+
+class _SearchViewControllerState extends State<SearchViewController> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
